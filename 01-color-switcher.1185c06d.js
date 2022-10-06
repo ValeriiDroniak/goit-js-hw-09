ac1a7f6c324080d837cef2b70ee23f115a6e4c36
ValeriiDroniak <105050454+ValeriiDroniak@users.noreply.github.com>
2022-10-06 12:16:59 +0000

    Deploying to gh-pages from @ ValeriiDroniak/goit-js-hw-09@95356d72df36c4b1d38831be10653a11c848be4e 🚀

---
+++ 01-color-switcher.1185c06d.js
@@ -0,0 +1,2 @@
+!function(){let t=null;const n={startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]")};function e(t){t.disabled=!0}function o(t){t.disabled=!1}function a(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}n.startBtn.addEventListener("click",(function(r){a(),t=setInterval(a,1e3),e(r.target),o(n.stopBtn)})),n.stopBtn.addEventListener("click",(function(a){clearInterval(t),e(a.target),o(n.startBtn)})),e(n.stopBtn)}();
+//# sourceMappingURL=01-color-switcher.1185c06d.js.map
